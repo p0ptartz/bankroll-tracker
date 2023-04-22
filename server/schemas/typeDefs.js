52e4e2ae1e646aaad59eaac8b175dd746cf34225
Eric Meyers <ericwm420@gmail.com>
2023-04-22 18:07:32 -0400

    added user typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
+type User {
+  _id: ID!
+  firstName: String!
+  lastName: String
+  email: String!
+  password: String!
+}
+
+type AuthPayload {
+    token: String!
+    expiresIn: Int!
+    user: User!
+  }
+
  type Entry {
   id: ID!
   date: String!
@@ -12,11 +27,15 @@ const typeDefs = gql`
   gameType: String!
 }
 
+
 type Query {
   entries: [Entry]
 }
 
 type Mutation {
+  signup(firstName: String!, lastName: String, email: String!, password: String!, profilePicture: String): AuthPayload
+  login(email: String!, password: String!): AuthPayload
+  logout: String
   addEntry(date: String!, location: String!, buyIn: Float!, cashOut: Float!, hours: Float!, stake: String!): Entry
 }
 `;

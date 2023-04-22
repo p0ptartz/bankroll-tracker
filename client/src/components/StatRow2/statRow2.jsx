5615c7b56ec6eff5f0343634b979e11a6f899b9d
Eric Meyers <ericwm420@gmail.com>
2023-04-21 23:11:03 -0400

    frontend basic layout

--- client/src/components/StatRow2/statRow2.jsx
+++ client/src/components/StatRow2/statRow2.jsx
@@ -7,7 +7,27 @@ export default function StatRow2() {
             <div className="graph-item-1 stat-item">
 
             </div>
-            <div className="most-recent stat-item">
+            <div className="most-recent-container">
+                <div className="most-recent ">
+                    <div className="recent-header">
+
+                    </div>
+                    <div className="recent-session">
+
+                    </div>
+                    <div className="recent-session">
+
+                    </div>
+                    <div className="recent-session">
+
+                    </div>
+                    <div className="recent-session">
+
+                    </div>
+                    <div className="recent-session">
+
+                    </div>
+                </div>
 
             </div>
         </>

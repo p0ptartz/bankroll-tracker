6a47e46f4976d2ef12c208acf7279137d70c4fea
Eric Meyers <ericwm420@gmail.com>
2023-04-22 00:30:07 -0400

    added stat layouts

--- client/src/components/StatRow1/statRow1.jsx
+++ client/src/components/StatRow1/statRow1.jsx
@@ -5,13 +5,22 @@ export default function StatRow1() {
     return (
         <>
             <div className="stat-item-1 stat-item">
-
+                <div className="mx-3 h-100 d-flex justify-content-between align-items-center total-hours">
+                    <p className="">Total Hours Played:</p>
+                    <p className="">555</p>
+                </div>
             </div>
             <div className="stat-item-2 stat-item">
-
+                <div className="mx-3 h-100 d-flex justify-content-between align-items-center total-hours">
+                    <p className="">Total Money Won:</p>
+                    <p className="">+25,500</p>
+                </div>
             </div>
             <div className="stat-item-3 stat-item">
-
+                <div className="mx-3 h-100 d-flex justify-content-between align-items-center total-hours">
+                    <p className="">Win %: <span>60%</span></p>
+                    <p className="">Hourly: <span>$150.20</span></p>
+                </div>
             </div>
         </>
 

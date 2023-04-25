0ab9d6364ddd695759e0e126d26c947e3e0d81f2
Eric Meyers <ericwm420@gmail.com>
2023-04-24 23:33:46 -0400

    successful login/signup

--- client/src/components/Landing/landingLogin/landingLogin.jsx
+++ client/src/components/Landing/landingLogin/landingLogin.jsx
@@ -1,9 +1,42 @@
 import { React, useState } from "react";
 import LandingBody from "../landingBody/landingBody";
+import { useMutation } from "@apollo/client"
+import { LOGIN_MUTATION } from "../../../utils/mutations/loginMutation";
+import { useNavigate } from "react-router-dom";
+
 
 function LandingLogin() {
-
+    const navigate = useNavigate()
     const [showBody, setShowBody] = useState(false);
+    const [login, { loading, error }] = useMutation(LOGIN_MUTATION)
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        // input values
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        try {
+            const { data } = await login({
+                variables: { email, password },
+            });
+
+            // testing the data in the console
+            console.log(data)
+            const userId = data.login._id;
+            console.log(userId)
+
+
+
+            console.log("WOOOO YOU DID IT")
+            // redirect to dashboard
+            navigate("/dashboard");
+
+        } catch (error) {
+            console.log("unlucky try again")
+        }
+    }
 
     const handleCloseButtonClick = () => {
         setShowBody(true);
@@ -18,14 +51,14 @@ function LandingLogin() {
             <div className="form-modal">
                 <div className="form-modal-container">
 
-                    <form id="loginForm">
+                    <form id="loginForm" onSubmit={handleLogin}>
                         <div className="exit-modal">
                             <h2>Login</h2>
                             <p className="close-btn" onClick={handleCloseButtonClick}>X</p>
                         </div>
 
-                        <input type="email" className="form-input" placeholder="Email*" required></input>
-                        <input type="password" className="form-input" placeholder="Password*" required></input>
+                        <input name="email" type="email" className="form-input" placeholder="Email*" required></input>
+                        <input name="password" type="password" className="form-input" placeholder="Password*" required></input>
                         <button type="submit" className="form-submit">Login</button>
                     </form>
                     <p id="toggleForm">Don't have an account? Sign Up</p>
---
+++ client/src/utils/mutations/loginMutation.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client"
+
+export const LOGIN_MUTATION = gql`
+mutation login($email: String!, $password: String!){
+login(email: $email, password: $password){
+    _id
+    email
+    password
+}
+}
+`

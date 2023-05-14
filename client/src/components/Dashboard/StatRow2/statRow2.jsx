a9993e34d094bfd35c4a4171cde0298b8d80ab64
Eric Meyers <ericwm420@gmail.com>
2023-05-13 22:36:53 -0400

    chart.js functionality started and populated

--- client/src/components/Dashboard/StatRow2/statRow2.jsx
+++ client/src/components/Dashboard/StatRow2/statRow2.jsx
@@ -1,47 +1,89 @@
 import React from "react";
-import "./statRow2.css"
+import "./statRow2.css";
 import { useQuery } from "@apollo/client";
 import { GET_USER_ENTRIES } from "../../../utils/queries/getUserEntriesQuery";
+import { Line } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    LineElement,
+    CategoryScale, // x axis
+    LinearScale, // y axis
+    PointElement
+} from 'chart.js'
 
+// LINE GRAPH ATTEMPT ref: https://www.youtube.com/watch?v=cxKNhSzxafg
 
-
+ChartJS.register(
+    LineElement,
+    CategoryScale,
+    LinearScale,
+    PointElement
+)
 
 
 function StatRow2() {
-    // console.log("stat row2")
-    const userId = localStorage.getItem('userId');
+    const userId = localStorage.getItem("userId");
     const { loading, error, data } = useQuery(GET_USER_ENTRIES, {
         variables: { userId },
     });
-    // console.log("user id here: " + userId)
-    // console.log("DATA!? " + JSON.stringify(data));
+
     const entries = data?.getUserEntries || [];
-    entries.forEach(entry => {
-        const date = entry.date;
-        // console.log("Date: " + date);
-        // You can perform further operations with the date value here
-    });
+    console.log("entries: " + entries)
 
     if (loading) {
-
         return <p>Loading...</p>;
     }
 
     if (error) {
-        console.log(error.message)
+        console.log(error.message);
         return <p>Error: {error.message}</p>;
-
-
-
     }
 
+    const recentSessions = entries.slice(0, 5);
+    const optionsChart = {
+        plugins: {
+            legend: true
+        },
+        scales: {
+            x: {
+                ticks: {
+                    beginAtZero: true
+                }
+            },
+            y: {
 
-    const recentSessions = entries.slice(0, 5)
+                ticks: {
+                    beginAtZero: true,
+
+                }
+            }
+        }
+    }
+    const dataChart = {
+        labels: ["", ...entries.map(entry => entry.date)],
+        datasets: [
+            {
+                label: 'Total Win/Loss',
+                data: [0, ...entries.reduce((accumulator, entry) => { // using spread operator to spread into new array
+                    const difference = entry.cashOut - entry.buyIn;
+                    const lastValue = accumulator.length > 0 ? accumulator[accumulator.length - 1] : 0;
+                    return [...accumulator, lastValue + difference];
+                }, [])],
+                backgroundColor: 'black',
+                borderColor: 'white',
+                pointBorderColor: 'white',
+            }
+        ]
+    };
 
     return (
         <>
             <div className="graph-item-1 stat-item">
-
+                <Line
+                    data={dataChart}
+                    options={optionsChart}
+                >
+                </Line>
             </div>
             <div className="most-recent-container">
                 <div className="most-recent ">
@@ -49,34 +91,41 @@ function StatRow2() {
                         <p className="mx-3">Recent Sessions:</p>
                     </div>
 
-
-
-                    {recentSessions.map(entry => {
-                        const location = entry.location
+                    {recentSessions.map((entry) => {
+                        const location = entry.location;
                         const capitalizedLocation = location
-                            .split(" ") // Split the string by spaces to get an array of words
-                            .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
-                            .join(" "); // Join the words back into a single string with spaces
-                        const date = entry.date
-                        const sum = entry.cashOut - entry.buyIn
+                            .split(" ")
+                            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                            .join(" ");
+                        const date = entry.date;
+                        const sum = entry.cashOut - entry.buyIn;
 
-                        return <div className="recent-session ">
-
-                            <div className="mx-3 h-100 d-flex justify-content-between align-items-center">
-                                <p className="recent-session-p">{capitalizedLocation}<br /> <span className="recent-session-location">Bensalem, PA</span> </p>
-                                <p className="recent-date"> <span className="">{date}</span> </p>
-                                <p className="win-loss"><span className={`${sum >= 0 ? 'green' : 'red'}`}>{sum}</span> </p>
+                        return (
+                            <div className="recent-session ">
+                                <div className="mx-3 h-100 d-flex justify-content-between align-items-center">
+                                    <p className="recent-session-p">
+                                        {capitalizedLocation}
+                                        <br />{" "}
+                                        <span className="recent-session-location">Bensalem, PA</span>{" "}
+                                    </p>
+                                    <p className="recent-date">
+                                        {" "}
+                                        <span className="">{date}</span>{" "}
+                                    </p>
+                                    <p className="win-loss">
+                                        <span className={`${sum >= 0 ? "green" : "red"}`}>{sum}</span>{" "}
+                                    </p>
+                                </div>
                             </div>
-
-                        </div>
+                        );
                     })}
-
-
                 </div>
-
             </div>
+
+
         </>
-    )
+    );
 }
 
-export default StatRow2
+
+export default StatRow2;

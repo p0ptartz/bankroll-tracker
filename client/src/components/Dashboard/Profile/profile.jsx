fa63ca19acc947e87550eb3a8b5ed4803ae66aea
Eric Meyers <ericwm420@gmail.com>
2023-05-18 22:29:41 -0400

    added some profile features

--- client/src/components/Dashboard/Profile/profile.jsx
+++ client/src/components/Dashboard/Profile/profile.jsx
@@ -7,19 +7,31 @@ function Profile() {
     const email = localStorage.getItem('email')
 
 
-
-
-
-
     return (
         <>
             <div className="profile-container p-5">
-                <p>WELCOME, </p>
-                <p>{userName}</p>
-                <p className="meh">{email}</p>
+                <div id="welcome" className="my-4">
+                    <p>WELCOME, </p>
+                    <p >{userName}</p>
+                    <p className="meh">{email}</p>
+                </div>
+                <div id="entries" className="my-4 ">
+                    <p className="p-1">Entries</p>
+                    <p className="p-1">Add an Entry</p>
+                    <p className="p-1">View Entries</p>
+                </div>
+                <div id="analytics" className="my-4 ">
+                    <p className="p-1">Analytics</p>
+                    <p className="p-1">Lifetime</p>
+                    <p className="p-1">Year</p>
+                    <p className="p-1">Month</p>
+                </div>
+
+
             </div>
         </>
     )
 }
+
 
 export default Profile

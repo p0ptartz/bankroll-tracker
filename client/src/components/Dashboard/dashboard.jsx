614e86cbb6753a4ec38bb9c0b6a843a948bc7d84
p0ptartz <ericwm420@gmail.com>
2023-05-22 19:02:50 -0400

    started fixing layout so profile doesnt scroll

--- client/src/components/Dashboard/dashboard.jsx
+++ client/src/components/Dashboard/dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./dashboard.css"
+import "./dashboard.css";
 import Profile from "./Profile/profile";
 import Nav from "./Nav/nav";
 import StatRow1 from "./StatRow1/statRow1";
@@ -8,15 +8,23 @@ import StatRow2 from "./StatRow2/statRow2";
 function Dashboard() {
     return (
         <>
-            <div className="dashboard-grid">
-                <Profile />
-                <Nav />
-                <StatRow1 />
-                <StatRow2 />
-            </div>
+            <div className="dash-container">
 
+
+                <div className="profile-g-container">
+                    <Profile />
+                </div>
+
+
+                <div className="dashboard-grid">
+                    <Nav />
+                    <StatRow1 />
+                    <StatRow2 />
+
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default Dashboard
+export default Dashboard;

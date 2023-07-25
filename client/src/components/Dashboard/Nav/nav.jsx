44025cd39c3eec38a239ee37063b7ea220c23b1c
Eric Meyers <ericwm420@gmail.com>
2023-07-24 21:53:54 -0400

    another fe bug fix

--- client/src/components/Dashboard/Nav/nav.jsx
+++ client/src/components/Dashboard/Nav/nav.jsx
@@ -101,78 +101,80 @@ const logout = () => {
 }
 
 return (
-    <section className="dashboard-nav ">
-        <div>
-            <h5 className="py-1"><span className="text-warning">deep</span><span className="text-white">pockets</span>
-            </h5>
-        </div>
-        <div className="text-end">
-            <p className=" logout py-1" onClick={logout} >Logout</p>
-        </div>
-        <div className="d-flex flex-column justify-content-end py-2">
-            <h2 className="text-white" >DASHBOARD</h2>
-            <p className="fs-6 text-primary" >Welcome to your dashboard!</p>
-        </div>
-        <div className="d-flex flex-column justify-content-end align-items-end py-3">
-            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="px-4 py-2 btn btn-primary">ADD A SESSION</button>
-        </div>
 
-        {/* ADD A SESSION MODAL */}
+    <>
+        <section className="dashboard-nav ">
+            <div>
+                <h5 className="py-1"><span className="text-warning">deep</span><span className="text-white">pockets</span>
+                </h5>
+            </div>
+            <div className="text-end">
+                <p className=" logout py-1" onClick={logout} >Logout</p>
+            </div>
+            <div className="d-flex flex-column justify-content-end py-2">
+                <h2 className="text-white" >DASHBOARD</h2>
+                <p className="fs-6 text-primary" >Welcome to your dashboard!</p>
+            </div>
+            <div className="d-flex flex-column justify-content-end align-items-end py-3">
+                <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="px-4 py-2 btn btn-primary">ADD A SESSION</button>
+            </div>
 
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable ">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="exampleModalLabel">Add A Session</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            {/* ADD A SESSION MODAL */}
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable ">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Add A Session</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleAddEntry}>
+                                <input type="date" name="date" id="" className="w-100 my-2 py-1" />
+                                <input type="text" name="location" placeholder="Location*" id="" className="w-100 my-2 py-1" />
+                                <input type="text" name="buyIn" placeholder="Total Buy In*" id="" className="w-100 my-2 py-1" />
+                                <input type="text" name="cashOut" placeholder="Cashed Out*" id="" className="w-100 my-2 py-1" />
+                                <input type="text" name="hours" placeholder="Hours Played*" id="" className="w-100 my-2 py-1" />
+                                <select name="stake" id="" className="w-100 my-2 py-1">
+                                    <option value="">Select a stake*</option>
+                                    <option value="1/2">1/2</option>
+                                    <option value="1/3">1/3</option>
+                                    <option value="2/2">2/2</option>
+                                    <option value="2/5">2/5</option>
+                                    <option value="5/5">5/5</option>
+                                    <option value="5/10">5/10</option>
+                                    <option value="10/10">10/10</option>
+                                    <option value="10/25">10/25</option>
+                                    <option value="25/25">25/25</option>
+                                    <option value="25/50">25/50</option>
+                                </select>
+                                <select name="gameType" id="" className="w-100 my-2 py-1">
+                                    <option value="">Select a Game Type*</option>
+                                    <option value="PLO">PLO</option>
+                                    <option value="NLHE">NLHE</option>
+                                    <option value="PLO5">PLO5</option>
+                                </select>
+                                <div className="modal-footer">
+
+
+                                </div>
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" className="btn btn-primary">Save changes</button>
+                            </form>
+                        </div>
+
                     </div>
-                    <div className="modal-body">
-                        <form onSubmit={handleAddEntry}>
-                            <input type="date" name="date" id="" className="w-100 my-2 py-1" />
-                            <input type="text" name="location" placeholder="Location*" id="" className="w-100 my-2 py-1" />
-                            <input type="text" name="buyIn" placeholder="Total Buy In*" id="" className="w-100 my-2 py-1" />
-                            <input type="text" name="cashOut" placeholder="Cashed Out*" id="" className="w-100 my-2 py-1" />
-                            <input type="text" name="hours" placeholder="Hours Played*" id="" className="w-100 my-2 py-1" />
-                            <select name="stake" id="" className="w-100 my-2 py-1">
-                                <option value="">Select a stake*</option>
-                                <option value="1/2">1/2</option>
-                                <option value="1/3">1/3</option>
-                                <option value="2/2">2/2</option>
-                                <option value="2/5">2/5</option>
-                                <option value="5/5">5/5</option>
-                                <option value="5/10">5/10</option>
-                                <option value="10/10">10/10</option>
-                                <option value="10/25">10/25</option>
-                                <option value="25/25">25/25</option>
-                                <option value="25/50">25/50</option>
-                            </select>
-                            <select name="gameType" id="" className="w-100 my-2 py-1">
-                                <option value="">Select a Game Type*</option>
-                                <option value="PLO">PLO</option>
-                                <option value="NLHE">NLHE</option>
-                                <option value="PLO5">PLO5</option>
-                            </select>
-                            <div className="modal-footer">
-
-
-                            </div>
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" className="btn btn-primary">Save changes</button>
-                        </form>
-                    </div>
-
                 </div>
             </div>
-        </div>
 
-        {/* BUTTON TO ADD SCRIPTED ENTRIES */}
-        {/* <button
+            {/* BUTTON TO ADD SCRIPTED ENTRIES */}
+            {/* <button
                 type="button"
                 className="px-4 py-2 session-btn"
                 onClick={fillEntries}
             >
                 FILL ENTRIES
             </button> */}
-    </section>
+        </section>
+    </>
 )
-}
